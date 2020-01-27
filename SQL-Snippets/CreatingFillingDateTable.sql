7b5ca6a6db29d551b732a2bb66bbc00d471e341b
Petter Emil Anderssen <pettereanderssen@gmail.com>
2020-01-27 15:29:39 +0100

    Added an if exist check, and null checks

--- SQL-Snippets/CreatingFillingDateTable.sql
+++ SQL-Snippets/CreatingFillingDateTable.sql
@@ -1,29 +1,31 @@
 --Creating the actual table
 
-CREATE TABLE [SCHEMA].[Calendar_Table](
-	[Day] [int] NOT NULL,
-	[Month] [int] NOT NULL,
-	[year] [int] NOT NULL,
-	[Day_Name] [nvarchar](32) NOT NULL,
-	[Month_Name] [nvarchar](32) NOT NULL,
-	[Date] [date] NOT NULL,
- CONSTRAINT [PK_Calendar_Table_1] PRIMARY KEY CLUSTERED 
-(
-	[Date] ASC
-)WITH (STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF) ON [PRIMARY]
-) ON [PRIMARY]
-GO
+IF OBJECT_ID('[SCHEMA].[Calendar_Table]') IS NULL --If table doesn't exist
+	BEGIN
+		CREATE TABLE [SCHEMA].[Calendar_Table](
+		[Day] [int] NOT NULL,
+		[Month] [int] NOT NULL,
+		[year] [int] NOT NULL,
+		[Day_Name] [nvarchar](32) NOT NULL,
+		[Month_Name] [nvarchar](32) NOT NULL,
+		[Date] [date] NOT NULL,
+		 CONSTRAINT [PK_Calendar_Table_1] PRIMARY KEY CLUSTERED 
+		(
+			[Date] ASC
+		)WITH (STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF) ON [PRIMARY]
+		) ON [PRIMARY]
+	END
 
 -- Filling the table with dates
 DECLARE @StartDate Date
 DECLARE @EndDate Date
 SET @StartDate = Cast('2019-01-01' As Date)
-SET @EndDate = Cast('2026-01-01' As Date)
+SET @EndDate = Cast('2031-01-01' As Date)
 
 --Looping over dates, adding a day every turn of the wheel
 WHILE @StartDate < @EndDate
       BEGIN
-             INSERT INTO [database].[Schema].[Calendar_Table]
+             INSERT INTO [DATABASE].[SCHEMA].[Calendar_Table]
              (
                    [Day], [Month], [Year], [Day_Name], [Month_Name], [Date]
              )
@@ -34,6 +36,7 @@ WHILE @StartDate < @EndDate
 				   DateName(weekday,@StartDate),
 				   DateName(Month,@StartDate),
 				   @StartDate
+			WHERE NOT Exists (SELECT * FROM [DATABASE].[SCHEMA].[Calendar_Table] WHERE [Date] = @StartDate)
 
              SET @StartDate = DATEADD(dd, 1, @StartDate) --Adding a day here
       END
